b90b638b59138e8838920a69723da2db4fe7c80a
Kevin L <kevin2022leung@gmail.com>
2022-08-24 02:35:21 -0400

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: "/search-locations/",
+  publicPath: "/search-locations/",
   transpileDependencies: true
 })
