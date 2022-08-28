3e774049561683ae294f799671f4113732c0a101
Kevin L <kevin2022leung@gmail.com>
2022-08-27 22:23:26 -0400

    Add checkbox and for selecting multiple locations

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="getLocation">Get current position</button>
+    <a-button type="default" @click="getLocation">Get current position</a-button>
     <div v-if="curPosResponse" class="cur-pos-response">{{ curPosResponse }}</div>
     <form id="search-form" @submit="submitSearch">
       <input 
@@ -9,7 +9,7 @@
         placeholder="Search Locations"
         v-model="searchText"
       >
-      <input type="submit" value="Search">
+      <a-button type="primary" @click="submitSearch">Search</a-button>
     </form>
     
     <!-- <table v-if="searchResponse.length > 0" id="search-results-table">
@@ -17,13 +17,20 @@
         <td>{{ location.place_name }}</td>
       </tr>
     </table> -->
+    <div class="delete-div" v-if="outputResults.length > 0" >
+      <a-button danger :disabled="!hasSelected" @click="deleteHandler">
+        Delete
+      </a-button>
+      <div v-if="hasSelected" class="num-of-selected">Selected {{ selectedRowKeys.length }} {{ (selectedRowKeys.length === 1 ? 'location' : 'locations') }}</div>
+    </div>
     <a-table 
-      v-if="filteredSearchResponse.length > 0" 
+      v-if="outputResults.length > 0" 
       id="search-results-table"
       :columns="tableColumns"
-      :data-source="filteredSearchResponse"
+      :data-source="outputResults"
+      :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
     />
-    <div v-else-if="filteredSearchResponse.length <= 0 && searched" class="no-search-results">No results</div>
+    <div v-else-if="outputResults.length <= 0 && searched" class="no-search-results">No results</div>
 
     <div id="map">
       <iframe 
@@ -45,13 +52,19 @@ export default {
       searchText: '',
       curPosResponse: false,
       searchResponse: [],
-      filteredSearchResponse: [],
+      outputResults: [],
       tableColumns: [
-        { title: 'Address', dataIndex: 'address', key: 'address' }
+        { title: 'Location', dataIndex: 'address', key: 'address' }
       ],
+      selectedRowKeys: [],
       searched: false,
       baseUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
       mapboxToken: 'pk.eyJ1Ijoia2V2aW44NTIiLCJhIjoiY2w3N3d3dHYzMDQ0YzNvcGx6b3Nndmg4NSJ9.3AuL3Enwv-Lt02i_8mDP4Q'
+    }
+  },
+  computed: {
+    hasSelected() {
+      return this.selectedRowKeys.length > 0
     }
   },
   methods: {
@@ -63,18 +76,18 @@ export default {
         .then((data) => {
           console.log('Searched: ', data)
           // this.searchResponse = data.features
-          this.filteredSearchResponse = []
-          console.log("🚀 ~ file: App.vue ~ line 71 ~ .then ~ this.filteredSearchResponse", this.filteredSearchResponse)
+          this.outputResults = []
+          console.log("🚀 ~ file: App.vue ~ line 71 ~ .then ~ this.outputResults", this.outputResults)
           for (const location of data.features) {
-            this.filteredSearchResponse.push({
+            this.outputResults.push({
               key: location.id,
               address: location.place_name,
             })
           }
-          console.log("this.filteredSearchResponse", this.filteredSearchResponse)
+          console.log("this.outputResults", this.outputResults)
         })
         .catch((error) => alert(error.message))
-      this.searched = true
+        this.searched = true
     },
     getLocation() {
       // alert('getLocation')
@@ -101,6 +114,14 @@ export default {
         alert('Geolocation not supported.')
       }
       
+    },
+    onSelectChange (selectedRowKeys) {
+      console.log('selectedRowKeys changed: ', selectedRowKeys)
+      this.selectedRowKeys = selectedRowKeys
+      console.log("🚀 ~ file: App.vue ~ line 121 ~ onSelectChange ~ this.selectedRowKeys", this.selectedRowKeys)
+    },
+    deleteHandler() {
+      console.log('^ Delete something...')
     }
   }
 }
@@ -124,15 +145,55 @@ body {
   height: 80vh;
 }
 
-#search-form, .cur-pos-response, #map, .no-search-results, #search-results-table {
+.cur-pos-response, table, ul.ant-pagination, .delete-div {
+  padding: 0 1rem;
+}
+
+#search-form, .cur-pos-response, #map, .no-search-results, #search-results-table, .delete-div {
   margin-top: 1rem;
+}
+
+#search-text {
+  display: inline;
+  width: 50vw;
+  height: 32px;
+  box-sizing: border-box;
+  margin: 0 2rem 0 0;
+  font-variant: tabular-nums;
+  list-style: none;
+  font-feature-settings: "tnum";
+  position: relative;
+  display: inline-block;
+  min-width: 0;
+  padding: 4px 11px;
+  color: #000000d9;
+  font-size: 14px;
+  line-height: 1.5715;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+  transition: all .3s;
+}
+
+#search-text:focus {
+  border-color: #40a9ff;
+  box-shadow: 0 0 0 2px rgba(24, 144, 255, .2);
+  border-right-width: 1px!important;
+  outline: 0;
+}
+
+.delete-div {
+  text-align: left;
 }
 
 table {
   margin-inline: auto;
 }
 
-.cur-pos-response, table, ul.ant-pagination {
-  padding: 0 1rem;
+.num-of-selected {
+  display: inline;
+  margin-left: 1rem;
 }
+
 </style>
